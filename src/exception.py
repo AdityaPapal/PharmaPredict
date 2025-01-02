bd008a6509c49a7033f266d842ed8730e150b11b
AdityaPapal <papaladitya@gmail.com>
2025-01-02 13:27:28 +0530

    done with logger,exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+from logger import logging 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
 
@@ -7,16 +7,25 @@ def error_message_detail(error,error_detail:sys):
     line_number = exc_tb.tb_lineno
     error_msg = str(error)
     
-    error_message = f"Error occured in script name [{file_name}] at line number [{line_number}] 
-    and error is [{error_msg}]"
+    error_message = "Error occured in script name [{0}] at line number [{1}] and error is [{2}]".format(
+        file_name,line_number,error_msg
+    )
 
     return error_message
 
 class CustomException(Exception):
 
     def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
+    
+
+if __name__=="__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)

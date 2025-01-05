d6a4df239808eaf0f027e64625db49bfe06a34eb
AdityaPapal <papaladitya@gmail.com>
2025-01-05 14:55:57 +0530

    done all backend

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -21,6 +21,15 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
+def load_model(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        logging.info("Exception occurs in load model function in utils")
+        raise CustomException(e,sys)
+
 def evaluate_model(x_train,y_train,x_test,y_test,models):
     try:
         report = {}
@@ -42,3 +51,6 @@ def evaluate_model(x_train,y_train,x_test,y_test,models):
     except Exception as e:
         logging.info("Exception occurs in evaluate model function in utils")
         raise CustomException(e,sys)
+    
+
+

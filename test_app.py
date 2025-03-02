d421c7d58b84245e6555d17193ee216a96e335c9
AdityaPapal <papaladitya@gmail.com>
2025-03-02 12:00:35 +0530

    update unit testing

--- test_app.py
+++ test_app.py
@@ -1,5 +1,9 @@
 import unittest
+
+import pandas as pd
+
 from app import app
+
 
 class PharmaPredictTestCase(unittest.TestCase):
 
@@ -12,27 +16,13 @@ class PharmaPredictTestCase(unittest.TestCase):
         """Test if home page loads correctly."""
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"<!DOCTYPE html>", response.data)  # Check if HTML content is loaded
+        self.assertIn(b"<!DOCTYPE html>", response.data) 
 
     def test_get_predict_page(self):
         """Test if prediction input form loads correctly."""
         response = self.app.get('/predict')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"<!DOCTYPE html>", response.data)  # Check if page has a relevant keyword
-
-     # Drug prediction page should load
-
-    # def test_post_predict_invalid_data(self):
-    #     """ Test POST request with invalid input data (e.g., missing fields)."""
-    #     response = self.app.post('/predict', data={
-    #         'Age': 'xyz',  # Invalid age input
-    #         'Sex': 'Male',
-    #         'BP': 'HIGH',
-    #         'Cholesterol': 'NORMAL',
-    #         'Na_to_K': '15.2'
-    #     })
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertIn(b"An error occurred during prediction", response.data)  # Should load the error page
+        self.assertIn(b"<!DOCTYPE html>", response.data)  
 
 if __name__ == "__main__":
     unittest.main()

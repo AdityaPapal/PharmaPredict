a84a9541289f2601370f8cf80b3fe8cbe09565c0
AdityaPapal <papaladitya@gmail.com>
2025-03-02 12:07:16 +0530

    update unit testing

--- test_app.py
+++ test_app.py
@@ -4,25 +4,15 @@ import pandas as pd
 
 from app import app
 
+def test_home_page(self):
+    """Test if home page loads correctly."""
+    response = self.app.get('/')
+    self.assertEqual(response.status_code, 200)
+    self.assertIn(b"<!DOCTYPE html>", response.data)
 
-class PharmaPredictTestCase(unittest.TestCase):
+def test_get_predict_page(self):
+    """Test if prediction input form loads correctly."""
+    response = self.app.get('/predict')
+    self.assertEqual(response.status_code, 200)
+    self.assertIn(b"<!DOCTYPE html>", response.data)
 
-    def setUp(self):
-        """ Set up the Flask test client."""
-        self.app = app.test_client()
-        self.app.testing = True
-
-    def test_home_page(self):
-        """Test if home page loads correctly."""
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b"<!DOCTYPE html>", response.data) 
-
-    def test_get_predict_page(self):
-        """Test if prediction input form loads correctly."""
-        response = self.app.get('/predict')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b"<!DOCTYPE html>", response.data)  
-
-if __name__ == "__main__":
-    unittest.main()
